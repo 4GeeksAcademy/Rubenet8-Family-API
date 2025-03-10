3ac33e9c17def0311b572620c768641a226a39f7
Rubeen.Frr8 <rubenferrermascarell@gmail.com>
2025-03-10 16:06:23 +0000

    Terminado

--- src/app.py
+++ src/app.py
@@ -1,55 +1,68 @@
-from flask import Flask, jsonify, request
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
+import os
+from flask import Flask, request, jsonify, url_for
+from flask_cors import CORS
+from utils import APIException, generate_sitemap
+from datastructures import FamilyStructure
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+CORS(app)
+jackson_family = FamilyStructure("Jackson")  # Create the jackson family object
 
 
-@app.route('/my-route', methods=['GET', 'POST', 'DELETE'])
-def my_route():
-    return '<h1 id="hector">Hello World!</h1>'
+# Handle/serialize errors like a JSON object
+@app.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
 
 
-@app.route('/todos', methods=['GET', 'POST'])
-def todos():
+# Generate sitemap with all your endpoints
+@app.route('/')
+def sitemap():
+    return generate_sitemap(app)
+
+
+@app.route('/members', methods=['GET', 'POST'])
+def handle_hello():
     response_body = {}
     if request.method == 'GET':
-      # json_text = jsonify(todos)
-      response_body['results'] = todos
-      response_body['message'] = 'Listado de TODOs'
-      return response_body, 200
+        members = jackson_family.get_all_members()
+        response_body['hello'] = "world"
+        response_body['family'] = members
+        return response_body, 200
     if request.method == 'POST':
-      data = request.json
-      if data == {}:
-        response_body['message'] = 'ERROR: TODO vacío'
-        response_body['results'] = {}
-        return response_body, 403
-      todos.append(data)
-      response_body['message'] = 'TODO agregado con éxito'
-      response_body['results'] = todos
-      return response_body, 201
-       
+        data = request.json
+        jackson_family.add_member(data)
+        members = jackson_family.get_all_members()
+        response_body['family'] = members
+        response_body['message'] = 'Nuevo integrante de la familia agregado'
+        return response_body, 200
 
-@app.route('/todos/<int:id>', methods=['GET', 'PUT', 'DELETE'])
-def todo(id):
+@app.route('/members/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def member(id):
     response_body = {}
-    # Validaciones
     if request.method == 'GET':
-        response_body['message'] = f'Datos del todo {id}'
-        response_body['results'] = {'label': 'todos'}
-        return response_body, 200
+        row = jackson_family.get_member(id)
+        if row:
+            response_body['results'] = row
+            response_body['message'] = 'Integrante encontrado'
+            return response_body, 200
+        response_body['message'] = f'Integrante con id {id} no encontrado'
+        return response_body, 402
     if request.method == 'PUT':
-        response_body['message'] = f'Actualización del todo {id}'
-        response_body['results'] = {'label': 'todos'}
-        return response_body, 200
+        pass
     if request.method == 'DELETE':
-        response_body['message'] = f'todo {id} eliminado'
-        response_body['results'] = {}
+        rows = jackson_family.delete_member(id)
+        response_body['family'] = rows
+        response_body['message'] = f'Integrante con id {id} eliminado'
         return response_body, 200
+    
 
-
-some_data = { 'name': 'Bobby', 'lastname': 'Rixer'}
-todos = [ { 'label': 'My first task', 'done': False}]
-
-
+# This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3245, debug=True)
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/utils.py
+++ src/utils.py
@@ -1,43 +1,39 @@
-from flask import jsonify, url_for
+"""
+Update this file to implement the following already declared methods:
+- add_member: Should add a member to the self._members list
+- delete_member: Should delete a member from the self._members list
+- update_member: Should update a member from the self._members list
+- get_member: Should return a member from the self._members list
+"""
+from random import randint
 
 
-class APIException(Exception):
-    status_code = 400
+class FamilyStructure:
+    def __init__(self, last_name):
+        self.last_name = last_name
+        self._members = [{'id': 1, 'first_name': 'John', 'last_name': self.last_name,'age': 33, 'lucky_number': [7, 13, 22]},
+                         {'id': self._generate_id(), 'first_name': 'Jane', 'last_name': self.last_name, 'age': 35, 'lucky_number': [10, 14, 32]},
+                         {'id': self._generate_id(), 'first_name': 'Jimmy', 'last_name': self.last_name, 'age': 5, 'lucky_number': [1]}]
 
-    def __init__(self, message, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
+    # Read-only: Use this method to generate random members ID's when adding members into the list
+    def _generate_id(self):
+        return randint(0, 99999999)
 
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        return rv
+    def add_member(self, member):
+        # Fill this method and update the return
+        member['id'] = self._generate_id()
+        member['last_name'] = self.last_name
+        self._members.append(member)
+        return
 
+    def delete_member(self, id):
+        self._members = [item for item in self._members if item['id'] != id]
+        return self._members
 
-def has_no_empty_params(rule):
-    defaults = rule.defaults if rule.defaults is not None else ()
-    arguments = rule.arguments if rule.arguments is not None else ()
-    return len(defaults) >= len(arguments)
+    def get_member(self, id):
+        member = [row for row in self._members if row['id'] == id]
+        return member
 
-
-def generate_sitemap(app):
-    links = []
-    for rule in app.url_map.iter_rules():
-        # Filter out rules we can't navigate to in a browser and rules that require parameters
-        if "GET" in rule.methods and has_no_empty_params(rule):
-            url = url_for(rule.endpoint, **(rule.defaults or {}))
-            links.append(url)
-    links_html = "".join(["<li><a href='" + y + "'>" + y + "</a></li>" for y in links])
-    return """
-        <div style="text-align: center;">
-            <img src='https://github.com/breatheco-de/exercise-family-static-api/blob/master/rigo-baby.jpeg?raw=true' />
-            <h1>Hello Rigo!!</h1>
-             This is your api home, remember to specify a real endpoint path like:
-             <ul style="text-align: left;">
-             """ + links_html + """
-            </ul>
-        </div>
-        """
+    # This method is done, it returns a list with all the family members
+    def get_all_members(self):
+        return self._members
